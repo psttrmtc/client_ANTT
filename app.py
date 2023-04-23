9f3f0d973f285bb046be8df2ba230b476d046bb5
minhloi12300@gmail.com <minhloi12300@gmail.com>
2023-04-24 01:31:32 +0700

    jwt

--- app.py
+++ app.py
@@ -11,16 +11,39 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+SERVER_URL = os.getenv("SERVER_URL")
+
+def get_access_token(username, password):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    payload = {
+        'username': username,
+        'password': password
+    }
+    response = requests.post(
+        f"{SERVER_URL}/login",
+        headers=headers,
+        data=json.dumps(payload),
+    )
+    if response.status_code != 200:
+        raise ValueError('Failed to send message')
+    return response.json()['access_token']
+
+access_token = get_access_token('user1', '123456')
+app.config['access_token'] = access_token
+
 
 def send_message(url, content):
     headers = {
+        "Authorization": f"Bearer {app.config['access_token']}",
         'Content-Type': 'application/json',
     }
     payload = {
         'messaging': content,
     }
-    response = requests.post(
-        url=url,
+    response = requests.get(
+        f"{url}/protected",
         headers=headers,
         data=json.dumps(payload),
     )
@@ -30,23 +53,19 @@ def send_message(url, content):
 
 @app.route("/", methods=['GET', 'POST'])
 def listen():
-    print(request.headers)
-    
+    print(app.config['access_token'])
     if request.method == 'POST':
-        host = request.headers.get('Referer')
-        
-        url = f'http://{host}/'
-        print("url: ",url)
-        payload = request.json
-        print("Request content ", payload)
-        # send_message(url, 'Message receive')
-        return jsonify({"message": "Hello, world!"})
+        content =  send_message(SERVER_URL,'Message receive')
+        return jsonify(content)
 
     else:
         return 'Unsupported request method.'
 
 def main():
-    app.run(port=8000)
+    access_token = get_access_token('user1', '123456')
+    print("Access Token: ", access_token)
+    
+    app.run(port=8000, debug=True)
 
 
 if __name__ == "__main__":
